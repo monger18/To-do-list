9dab644482503eb7938dca03d2e0e0edf367fed6
Satyam srinath <130518559+monger18@users.noreply.github.com>
2023-05-06 21:30:54 +0530

    Add files via upload

---
+++ ToDo-list/script.js
@@ -0,0 +1,44 @@
+let addtask=document.getElementById('add-task');
+let taskcontainer=document.getElementById('task-container');
+let inputtask=document.getElementById('input-task');
+
+addtask.addEventListener('click',function(){
+    let task=document.createElement('div');
+    task.classList.add('task');
+
+    let li=document.createElement('li')
+    li.innerText=`${inputtask.value}`;
+    task.appendChild(li);
+
+
+    let checkbutton=document.createElement("button");
+    checkbutton.innerHTML='<i class="fa-solid fa-check"></i>';
+    checkbutton.classList.add('checkTask');
+    task.appendChild(checkbutton);
+
+    
+    let deletebutton=document.createElement("button");
+    deletebutton.innerHTML='<i class="fa-solid fa-trash-can"></i>';
+    deletebutton.classList.add('deleteTask');
+    task.appendChild(deletebutton);
+
+    if(inputtask.value==""){
+        alert('Please enter the task');
+    }
+    else{
+        taskcontainer.appendChild(task);
+    }
+
+    inputtask.value="";
+
+
+
+    checkbutton.addEventListener('click', function(){
+        checkbutton.parentElement.style.textDecoration="line-through";
+    })
+
+    deletebutton.addEventListener('click',function(e){
+          let target=e.target;
+          target.parentElement.parentElement.remove();
+    })
+});
